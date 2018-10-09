c20372dfc47d347d4bb265ea8ec4244b677aad30
Nate Fleming <nfleming@ksu.edu>
2018-10-08 20:27:04 -0500

    Updated read_heat_balance read_user_input and read_arrow_table functions with addition steps and inputs/outputs

--- src/main.py
+++ src/main.py
@@ -13,10 +13,13 @@ Created on Wed Oct  3 13:19:33 2018
 
 import pandas as pd
 import numpy as np
+import os
+import pressure_temp_combo
 
-def read_heat_balance(excelfilename,case=None):
+
+def read_heat_balance(mcInput_hb5_hb, case=None):
     # load the spreadsheet
-    df = pd.read_excel(excelfilename)
+    df = pd.read_excel(mcInput_hb5_hb)
     # replace nan with blanks
     df = df.replace(np.nan, '')
     # rename indexes to the rel indexes
@@ -30,14 +33,14 @@ def read_heat_balance(excelfilename,case=None):
     if case is not None:    
         DictCase = Dict[case]
     
-    return df,Dict,DictCase
+    return df,Dict,DictCase,massflow_design,pressure,temperature
     
-def read_user_inputs(P = 1e5 ,T = 300):
+def read_user_inputs(target_cfr = None, plant_config = None, NPS = None, schedule = None):
     '''
     Describe
     '''
-    
-    return 2*P, 5*T
+    pressure_temp_combo(.....)
+    return vel_min_sb
 
 def read_pipe_data(filename, what_to_read=None):
     '''
@@ -48,6 +51,18 @@ def read_pipe_data(filename, what_to_read=None):
     return out1,out2
 
 def read_arrow_tables(filename):
+    '''
+    Describe
+    '''
+    #Opens Arrow through given executable path of Arrow
+    os.sys('arrow main.py')
+    #Arrow opens pipe file
+    
+    #Arrow saves data as .csv
+    
+    return csv
+
+def steam_table_lookups():
     '''
     Describe
     '''
@@ -65,7 +80,11 @@ def make_outputs():
 if __name__ == '__main__':
     df = read_heat_balance("..\inputs\mcInput_hb5.0_hb.xlsx")
     df,Dict,DictCase = read_heat_balance("..\inputs\mcInput_hb5.0_hb.xlsx","Case S-025-G")
-    
+    csv1 = read_arrow_tables(filename)
+    # read NPS, schedule from csv
+    NPS=..
+    schedule = ..
+    read_user_inputs(,,NPS,schedule)
     #x,y = read_user_inputs(2e5,300)
     #m,n = read_pipe_data('/home/mgabdo/ME575/Kiewit/inputs/pipedata/foo.txt')
     #tbl = read_arrow_tables('/loc/of/tables/spitted/by/arrow')
